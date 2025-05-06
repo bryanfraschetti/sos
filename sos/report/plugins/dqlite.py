a49b08d8f88dc8a7ffbb654b30c6995a8da19cdf
Bryan Fraschetti <bryan.fraschetti@canonical.com>
2025-05-06 16:45:53 -0400

    [dqlite] Created new plugin for dqlite
    This commit adds a new plugin for dqlite which can be triggered by the
    several projects that leverage its capabilities such as microcloud,
    microceph, microovn, microk8s, and lxd. As it is an embedded db, each
    application uses it slightly differently - ie. different tables,
    different columns, etc. Common collection points between the activators
    are placed in the baseCollection function while application-specific
    collections go under the eponymous collector functions, some of which
    are no-ops as of right now. The command output is stored under
    dqlite/{activator}/ to categorize and organize the collection since one
    or more of the activators may be present on the system and each should
    be collected.
    
    The setup function runs the general collection and the
    application-specific collector to generate a list of applicable queries
    to run against dqlite. For most of the activators, these are then run
    against the db in two ways - using the built in commands some
    applications offer (eg. microceph cluster sql) as well as by curling the
    unix socket. These are then executed in the run_batch_dqlite_queries
    function.
    
    Signed-off-by: Bryan Fraschetti <bryan.fraschetti@canonical.com>

---
+++ sos/report/plugins/dqlite.py
@@ -0,0 +1,298 @@
+# Copyright (C) 2023 Canonical Ltd.,
+#                    Bryan Fraschetti <bryan.fraschetti@canonical.com>
+
+# This file is part of the sos project: https://github.com/sosreport/sos
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions of
+# version 2 of the GNU General Public License.
+#
+# See the LICENSE file in the source distribution for further information.
+
+import json
+import re
+from sos.report.plugins import Plugin, IndependentPlugin, SoSPredicate
+
+
+class dqlite(Plugin, IndependentPlugin):
+    """Dqlite (distributed SQLite) extends SQLite across a cluster of machines,
+    with automatic failover and high-availability to keep your application
+    running. It uses C-Raft, an optimised Raft implementation in C, to gain
+    high-performance transactional consensus and fault tolerance while
+    preserving SQlite’s outstanding efficiency and tiny footprint.
+    """
+
+    short_desc = 'Distributed embedded sqlite database library'
+    plugin_name = "dqlite"
+    profiles = ('storage', 'cluster',)
+
+    packages = ('microk8s', 'microceph', 'microovn', 'microcloud', 'lxd',)
+
+    def postproc(self):
+
+        # Remove microk8s certificate data from config file
+        protect_keys = [
+            "certificate-authority-data",
+            "client-certificate-data",
+            "client-key-data",
+        ]
+
+        regexp = fr"^\s*(#?\s*({'|'.join(protect_keys)}):\s*)(\S.*)"
+
+        self.do_file_sub(
+            "/var/snap/microk8s/current/credentials/client.config",
+            regexp,
+            r"\1 ******",
+        )
+
+    def run_batch_dqlite_queries(self, pkg, cfg, queries):
+        sql_cmd = cfg.get("sql_cmd")
+        sock = cfg.get("socket")
+        predicate = cfg.get("predicate")
+        endpoint = cfg.get("socket_endpoint")
+        header = "Content-Type: application/json"
+
+        curl_cmd = f"""
+            curl -s --unix-socket {sock} \
+                -X POST {endpoint} \
+                -H \"{header}\" \
+                -d
+            """
+
+        for query_entry in queries:
+            query = query_entry.get("query")
+            table_name = query_entry.get("table")
+            query_json = json.dumps(
+                {"query": query, "database": "local"} if pkg == "lxd"
+                else {"query": query}
+            )
+            socket_cmd = f"{curl_cmd} '{query_json}'"
+
+            self.add_cmd_output(
+                socket_cmd,
+                suggest_filename=f"{pkg}_dqlite_{table_name}",
+                subdir=pkg,
+                pred=predicate,
+            )
+
+            self.add_cmd_output(
+                f"{sql_cmd} {json.dumps(query)}",
+                suggest_filename=f"{sql_cmd}_{table_name}",
+                subdir=pkg,
+                pred=predicate
+            )
+
+    def base_collection(self, pkg, cfg):
+        db_path = cfg.get("db_path")
+
+        # Check for inconsistent dqlite db intervals
+        self.add_dir_listing(
+            db_path,
+            suggest_filename=f"ls_{pkg}_dqlite_dir",
+            subdir=pkg,
+        )
+
+        # All dqlite consumers except lxd have info.yaml and cluster.yaml
+        self.add_copy_spec(
+            [
+                f"{db_path}/info.yaml",
+                f"{db_path}/cluster.yaml",
+                f"{db_path}/../daemon.yaml",  # Not expected for microk8s
+            ]
+        )
+
+        # Determine queries to run based on installed package
+        if pkg == "microk8s":
+            # At this time, microk8s is rather divergent w.r.t querying
+            return
+
+        queries = [{
+            "query": "SELECT * FROM sqlite_master WHERE type=\"table\";",
+            "table": "schema",  # Table name will be used in filename
+        },]
+
+        if pkg not in ("lxd",):
+            queries.extend([
+                {
+                    "query": (
+                        "SELECT id, name, expiry_date "
+                        "FROM core_token_records;"
+                    ),
+                    "table": "token_records",
+                },
+                {
+                    "query": (
+                        "SELECT id, name, address, schema_internal, "
+                        "schema_external, heartbeat, role, api_extensions "
+                        "FROM core_cluster_members;"
+                    ),
+                    "table": "core_cluster_members",
+                },
+            ])
+
+        if pkg not in ("microcloud",):
+            queries.append({
+                "query": (
+                    "SELECT * FROM config WHERE NOT ( "
+                    "key LIKE \"%keyring%\" OR "
+                    "key LIKE \"%ca_cert%\" OR "
+                    "key LIKE \"%ca_key%\" );"
+                ),
+                "table": "config",
+            })
+
+        if pkg in ("microceph", "microovn",):
+            queries.append({
+                "query": "SELECT * FROM services;",
+                "table": "services",
+            })
+
+        self.run_batch_dqlite_queries(pkg, cfg, queries)
+
+    def microceph_collection(self, pkg, cfg):
+        queries = [
+            {
+                "query": "SELECT * FROM disks;",
+                "table": "disks",
+            },
+            {
+                "query": "SELECT * FROM client_config;",
+                "table": "client_config",
+            },
+            {
+                "query": "SELECT * FROM remote;",
+                "table": "remote",
+            },
+        ]
+
+        self.run_batch_dqlite_queries(pkg, cfg, queries)
+
+    def microovn_collection(self, pkg, cfg):
+        """ Currently empty, as nothing currently necessitates unique microovn
+        dqlite collection. A no-op, present for future extension
+        """
+
+    def microcloud_collection(self, pkg, cfg):
+        """ Currently empty, as nothing currently necessitates unique
+        microcloud dqlite collection. A no-op, present for future extension
+        """
+
+    def microk8s_collection(self, pkg, cfg):
+        db_path = cfg.get("db_path")
+        predicate = cfg.get("predicate")
+
+        self.add_copy_spec([
+            "/var/snap/microk8s/current/credentials/client.config",
+            f"{db_path}/failure-domain",
+        ])
+
+        dqlite_bin = "/snap/microk8s/current/bin/dqlite"
+        cert = f"{db_path}/cluster.crt"
+        key = f"{db_path}/cluster.key"
+        servers = f"{db_path}/cluster.yaml"
+        dqlite_cmd = f"{dqlite_bin} -c {cert} -k {key} -s file://{servers} k8s"
+
+        queries = [
+            "\".cluster\"",
+            "\".cluster\" -f json",
+            "\".leader\"",
+        ]
+
+        suggested_names = [
+            f"{pkg}_dqlite_{query}" for query in queries
+        ]
+
+        try:
+            with open(servers, 'r', encoding='utf-8') as cluster_definition:
+                cluster = cluster_definition.read()
+                nodes = re.findall(
+                    r'Address:\s*(\d+\.\d+\.\d+\.\d+:\d+)', cluster
+                )
+
+                for node in nodes:
+                    queries.append(
+                        f"\".describe {node}\" -f json"
+                    )
+                    suggested_names.append(
+                        f"{pkg}_dqlite_.describe_{node}"
+                    )
+        except Exception as e:
+            self.add_alert(f"Failed to parse {servers}: {e}")
+
+        for query, suggested_name in zip(queries, suggested_names):
+            self.add_cmd_output(
+                f"{dqlite_cmd} {query}",
+                suggest_filename=suggested_name,
+                subdir=pkg,
+                pred=predicate,
+            )
+
+    def lxd_collection(self, pkg, cfg):
+        """ Currently empty, as nothing currently necessitates unique
+        lxd dqlite collection. A no-op, present for future extension
+        """
+
+    def setup(self):
+        lxd_pred = (
+            SoSPredicate(
+                self,
+                services=['snap.lxd.daemon'],
+                required={'services': 'all'}
+            ) if self.is_snap_installed("lxd") else
+            SoSPredicate(
+                self,
+                services=['lxd'],
+                required={'services': 'all'}
+            )
+        )
+
+        packages = {
+            "microceph": {
+                "db_path": "/var/snap/microceph/common/state/database",
+                "socket": "/var/snap/microceph/common/state/control.socket",
+                "sql_cmd": "microceph cluster sql",
+                "socket_endpoint": "microceph/core/internal/sql",
+                "collection": self.microceph_collection,
+                "predicate": None,
+            },
+            "microovn": {
+                "db_path": "/var/snap/microovn/common/state/database",
+                "socket": "/var/snap/microovn/common/state/control.socket",
+                "sql_cmd": "microovn cluster sql",
+                "socket_endpoint": "microovn/core/internal/sql",
+                "collection": self.microovn_collection,
+                "predicate": None,
+            },
+            "microcloud": {
+                "db_path": "/var/snap/microcloud/common/state/database",
+                "socket": "/var/snap/microcloud/common/state/control.socket",
+                "sql_cmd": "microcloud sql",
+                "socket_endpoint": "microcloud/core/internal/sql",
+                "collection": self.microcloud_collection,
+                "predicate": None,
+            },
+            "microk8s": {
+                "db_path": "/var/snap/microk8s/current/var/kubernetes/backend",
+                "socket": "/var/snap/microk8s/current/var/kubernetes/backend/"
+                "kine.sock:12379",
+                "sql_cmd": None,
+                "socket_endpoint": "microk8s/core/internal/sql",
+                "collection": self.microk8s_collection,
+                "predicate": None,
+            },
+            "lxd": {
+                "db_path": "/var/snap/lxd/common/lxd/database/global",
+                "socket": "/var/snap/lxd/common/lxd/unix.socket",
+                "sql_cmd": "lxd sql local",
+                "socket_endpoint": "lxd/internal/sql",
+                "collection": self.lxd_collection,
+                "predicate": lxd_pred,
+            }
+        }
+
+        for pkg, config in packages.items():
+            if self.is_installed(pkg):
+                self.base_collection(pkg, config)
+                config.get("collection")(pkg, config)
+
+# vim: set et ts=4 sw=4 :
